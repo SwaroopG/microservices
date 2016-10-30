0c69d3fad352de0d20d5f8d5618f6c27a6167232
sgaddameedhi <sgaddameedhi@pulseinc.com>
2016-10-29 19:59:03 -0500

    Eureka Server is ready

---
+++ eureka-server/src/main/java/com/poorjar/eureka/EurekaServer.java
@@ -0,0 +1,16 @@
+package com.poorjar.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+    public static void main(String[] args) {
+        SpringApplication eurekaServer = new SpringApplication(EurekaServer.class);
+        eurekaServer.addListeners(new ApplicationPidFileWriter("eureka-server.pid"));
+        eurekaServer.run(args);
+    }
+}
